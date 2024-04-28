495763333fc45581a2de514516cb598cfc937c6c
Tessa Delsener <wildtessa.70@icloud.com>
2024-04-28 07:48:24 -0700

    Initial Commit

---
+++ Questions App/Questions_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Questions_AppApp.swift
+//  Questions App
+//
+//  Created by Tessa Delsener on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Questions_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
